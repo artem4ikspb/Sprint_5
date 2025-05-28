1ba48438c12dadb2e39ff99c902f9db08c6e9c4e
ArtemK <artem4ik.spb@yandex.ru>
2025-05-28 23:29:58 +0300

    fix create adv test

--- data/test_data.py
+++ data/test_data.py
@@ -1,4 +1,4 @@
 red_color = "rgb(255, 105, 114)"
 login_data_success = {'email': 'god@y.co', 'password': 'Qwerty123'}
 
-units = [{'name': 'Рапира', 'description': 'Учебная', 'price': 4000, 'city': 'Казань', 'condition':'used'}]
+units = [{'name': 'Шпага', 'description': 'Учебная', 'price': 8000, 'city': 'Казань', 'condition':'new'}]
--- tests/test_create_ad.py
+++ tests/test_create_ad.py
@@ -12,86 +12,25 @@ from selenium.webdriver.support.wait import WebDriverWait
 from utils.helpers import get_random_unit
 
 
+URL = 'https://qa-desk.stand.praktikum-services.ru/'
+
+
 class TestCreateAds:
-    def main_page(self, driver):
-        return driver.find_element(*MPL.MAIN_PAGE)
-
-    def login_reg_button(self,driver):
-        return driver.find_element(*MPL.LOGIN_AND_REG_BUTTON)
-
-    def create_advertisement_click(self, driver):
-        button = WebDriverWait(driver, 5).until(EC.element_to_be_clickable(APL.CREATE_AD_BUTTON))
-        button.click()
-        return driver.find_element(*APL.AD_CREATE_PAGE)
-    
-    def open_unathorized_modal_form(self,driver):
-        return WebDriverWait(driver, 3).until(EC.presence_of_element_located(APL.NEED_AUTH_MODAL_FORM))
-    
-    def login(self, driver, login_data):
-        email = WebDriverWait(driver, 3).until(EC.element_to_be_clickable(LMF.EMAIL_LOGIN_INPUT))
-        email.send_keys(login_data.get("email"))
-        driver.find_element(*LMF.PASSWORD_LOGIN_INPUT).send_keys(login_data.get("password"))
-        driver.find_element(*LMF.LOGIN_BUTTON).click()
-        WebDriverWait(driver, 3).until(EC.staleness_of(email))
-    
-    def fill_text_fields(self, driver, unit):
-        driver.find_element(*APL.UNIT_NAME).send_keys(unit["name"])
-        driver.find_element(*APL.UNIT_DESCRIPTION).send_keys(unit["description"])
-        driver.find_element(*APL.UNIT_PRICE).send_keys(str(unit["price"]))
-
-    def fill_drop_box(self, driver):
-        dropdown_button = WebDriverWait(driver, 3).until(EC.element_to_be_clickable(APL.CATEGORY_DROPDOWN_BUTTON))
-        dropdown_button.click()
-        element = WebDriverWait(driver, 3).until(EC.element_to_be_clickable(APL.CATEGORY_DROPDOWN_BUTTON_HOBBY))
-        element.click()
-        dropdown_button = WebDriverWait(driver, 3).until(EC.element_to_be_clickable(APL.CITY_DROPDOWN_BUTTON))
-        dropdown_button.click()
-        element = WebDriverWait(driver, 3).until(EC.element_to_be_clickable(APL.CITY_DROPDOWN_BUTTON_KAZAN))
-        element.click()
-
-    def set_unit_condition(self, driver, unit):
-        if unit.get("condition") == "new":
-            return driver.find_element(*APL.CONDITION_RADIOBUTTON_NEW)
-        return driver.find_element(*APL.CONDITION_RADIOBUTTON_USED)
-
-    def publish_button_click(self, driver):
-        driver.find_element(*APL.PUBLISH_AD_BUTTON).click()
-        WebDriverWait(driver, 3).until(EC.staleness_of(driver))
-
-    def open_user_profile(self, driver):
-        driver.find_element(*MPL.AVATAR_IMAGE).click()
-        return WebDriverWait(driver, 3).until(EC.presence_of_element_located(PPL.USER_PROFILE_PAGE))
-
-    def get_ad_info_by_title(self, driver, card_name):
-        commond_ads_list = driver.find_element(*PPL.USER_PROFILE_PAGE_GRID)
-        card = commond_ads_list.find_element(*PPL.user_profile_ad_card(card_name))
-        name = card.find_element(*PPL.USER_PROFILE_AD_NAME).text
-        city = card.find_element(*PPL.USER_PROFILE_AD_CITY).text
-        price_text = card.find_element(*PPL.USER_PROFILE_AD_PRICE).text
-        price = int(price_text.replace("₽", "").replace(" ", "").strip())
-        return name, city, price
-
-    def get_count_ads_with_name(self, driver, card_name):
-        commond_ads_list = driver.find_element(*PPL.USER_PROFILE_PAGE_GRID)
-        cards = commond_ads_list.find_elements(*PPL.user_profile_ad_card(card_name))
-        return len(cards)
-
-    def is_unathorized_modal_form_shown(self, driver):
-        try:
-            self.open_unathorized_modal_form(driver)
-        except NoSuchElementException:
-            return False
-        return True
-        
     # Создание объявления неавторизованным пользователем
     # Что нужно сделать: 
     # Нажать кнопку «Разместить объявление».
     # Проверить: отображается модальное окно с заголовком «Чтобы разместить объявление, авторизуйтесь».
     def test_create_ad_without_auth(self, driver: WebDriver):
-        self.create_advertisement_button(driver).click()
-        assert self.is_unathorized_modal_form_shown(driver)
-
-
+        driver.get(URL)
+        button = WebDriverWait(driver, 3).until(
+            EC.element_to_be_clickable(APL.CREATE_AD_BUTTON),
+            message=f"Can't find element by locator {APL.CREATE_AD_BUTTON}")
+        button.click()
+        # driver.find_element(*APL.AD_CREATE_PAGE)
+        modal_form = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(APL.NEED_AUTH_MODAL_FORM),
+            message=f"Can't find element by locator {APL.NEED_AUTH_MODAL_FORM}")
+        assert modal_form
 
     # Создание объявления авторизованным пользователем
     # Что нужно сделать: 
@@ -105,22 +44,62 @@ class TestCreateAds:
     #@pytest.mark.skip
     def test_create_ad_success(self, driver: WebDriver):
         unit = units[0]
-        self.login_reg_button(driver).click()
-        self.login(driver, login_data_success)
-        profile_page = self.open_user_profile(driver)
-        count_before_add_unit = self.get_count_ads_with_name(profile_page, unit.get('name'))
-        adv_page = self.create_advertisement_click(driver)
-        self.fill_text_fields(adv_page, unit)
-        self.fill_drop_box(adv_page)
-        self.set_unit_condition(adv_page, unit).click()
-        self.publish_button_click(adv_page)
-        profile_page = self.open_user_profile(driver)
-        count_after_add_unit = self.get_count_ads_with_name(profile_page, unit.get('name'))
-        name, city, price = self.get_ad_info_by_title(profile_page, unit.get('name'))
+        driver.get(URL)
+        button = WebDriverWait(driver, 1).until(
+            EC.element_to_be_clickable(MPL.LOGIN_AND_REG_BUTTON),
+            message=f"Can't find element by locator {MPL.LOGIN_AND_REG_BUTTON}")
+        button.click()
+    
+        login_modal_form = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(LMF.LOGIN_FORM_MODAL),
+            message=f"Can't find element by locator {LMF.LOGIN_FORM_MODAL}")
+        login_modal_form.find_element(*LMF.EMAIL_LOGIN_INPUT).send_keys(login_data_success.get("email"))
+        login_modal_form.find_element(*LMF.PASSWORD_LOGIN_INPUT).send_keys(login_data_success.get("password"))
+        login_modal_form.find_element(*LMF.LOGIN_BUTTON).click()
+        WebDriverWait(login_modal_form, 3).until(EC.staleness_of(login_modal_form))
+
+        driver.find_element(*APL.CREATE_AD_BUTTON).click()
+        adv_page = WebDriverWait(driver, 3).until(EC.presence_of_element_located(APL.AD_CREATE_PAGE))
+        adv_page.find_element(*APL.UNIT_NAME).send_keys(unit["name"])
+        adv_page.find_element(*APL.UNIT_DESCRIPTION).send_keys(unit["description"])
+        adv_page.find_element(*APL.UNIT_PRICE).send_keys(str(unit["price"]))
+
+        dropdown_button = adv_page.find_element(*APL.CATEGORY_DROPDOWN_BUTTON)
+        dropdown_button.click()
+        element = WebDriverWait(adv_page, 3).until(
+            EC.element_to_be_clickable(APL.CATEGORY_DROPDOWN_BUTTON_HOBBY),
+            message=f"Can't find element by locator {APL.CATEGORY_DROPDOWN_BUTTON_HOBBY}")
+        element.click()
+        dropdown_button = adv_page.find_element(*APL.CITY_DROPDOWN_BUTTON)
+        dropdown_button.click()
+        element = WebDriverWait(adv_page, 3).until(
+            EC.element_to_be_clickable(APL.CITY_DROPDOWN_BUTTON_KAZAN),
+            message=f"Can't find element by locator {APL.CITY_DROPDOWN_BUTTON_KAZAN}")
+        element.click()
+        if unit.get("condition") == "new":
+            adv_page.find_element(*APL.CONDITION_RADIOBUTTON_NEW).click
+        else: adv_page.find_element(*APL.CONDITION_RADIOBUTTON_USED).click
+        adv_page.find_element(*APL.PUBLISH_AD_BUTTON).click()
+        WebDriverWait(adv_page, 3).until(EC.staleness_of(adv_page))
+        # driver.find_element(*MPL.MAIN_PAGE)
+
+        driver.find_element(*MPL.AVATAR_IMAGE).click()
+        profile_page = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(PPL.USER_PROFILE_PAGE),
+            message=f"Can't find element by locator {PPL.USER_PROFILE_PAGE}")
+
+        commond_ads_list = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(PPL.USER_PROFILE_PAGE_GRID),
+            message=f"Can't find element by locator {PPL.USER_PROFILE_PAGE_GRID}")
+        card = commond_ads_list.find_element(*PPL.user_profile_ad_card(unit.get('name')))
+        name = card.find_element(*PPL.USER_PROFILE_AD_NAME).text
+        city = card.find_element(*PPL.USER_PROFILE_AD_CITY).text
+        price_text = card.find_element(*PPL.USER_PROFILE_AD_PRICE).text
+        price = int(price_text.replace("₽", "").replace(" ", "").strip())
+
         assert name == unit.get('name'), f"Название товара > {name}, не соответствует ожидаемому > {unit.get('name')}"
         assert city == unit.get('city'), f"Название города > {city}, не соответствует ожидаемому > {unit.get('city')}"
         assert price == unit.get('price'), f"Цена > {price}, не соответствует ожидаемой > {unit.get('price')}"
-        assert count_before_add_unit+1 == count_after_add_unit
 
 
 
